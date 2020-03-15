543b900873f127ded8e6e6fb70554a1c0cbc20ce
Marcelo <marcelo@marceloweb.info>
2020-03-14 21:30:02 -0300

    - rename bucket

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ s3 = boto3.resource('s3')
 
 def list_images():
     images = []
-    bucket = s3.Bucket('fa-imagens')
+    bucket = s3.Bucket('fa-images-lab')
     for images in bucket.objects.all():
         images.append(image.key)
     print(images)
